40069a3a1060987bf453ca8e6eeeac7e2e3e26ef
Ali Eren YAMAN <65658818+ErgeSooN@users.noreply.github.com>
2026-07-06 14:32:22 +0300

    Implement SMTP email sending in debug-smtp.php
    Added SMTP email sending functionality with debug output.

--- public_html/debug-smtp.php
+++ public_html/debug-smtp.php
@@ -2,17 +2,13 @@
 header('Content-Type: text/plain');
 $config = @include __DIR__ . '/includes/smtp-config.php';
 echo "config loaded: " . (is_array($config) ? 'yes' : 'no') . "\n";
-if (is_array($config)) {
-    echo "host: " . $config['host'] . "\n";
-    echo "port: " . $config['port'] . "\n";
-    echo "username: " . $config['username'] . "\n";
-    echo "password length: " . strlen($config['password']) . "\n";
-}
 
 $host = $config['host'];
 $port = (int) $config['port'];
 $username = $config['username'];
 $password = $config['password'];
+$from = $config['from'] ?? $username;
+$to = 'mail@alierenyaman.com';
 
 $target = ($port === 465) ? 'ssl://' . $host : $host;
 $fp = @stream_socket_client($target . ':' . $port, $errno, $errstr, 15);
@@ -44,5 +40,21 @@ echo "USERNAME STEP: " . readAll($fp);
 fwrite($fp, base64_encode($password) . "\r\n");
 echo "PASSWORD STEP: " . readAll($fp);
 
+fwrite($fp, "MAIL FROM: <$from>\r\n");
+echo "MAIL FROM: " . readAll($fp);
+
+fwrite($fp, "RCPT TO: <$to>\r\n");
+echo "RCPT TO: " . readAll($fp);
+
+fwrite($fp, "DATA\r\n");
+echo "DATA: " . readAll($fp);
+
+$headers = "From: $from\r\nTo: $to\r\nSubject: Test debug\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=UTF-8\r\n";
+$body = "Bu bir debug test mailidir.";
+$message = $headers . "\r\n" . $body . "\r\n.";
+fwrite($fp, $message . "\r\n");
+echo "SEND BODY: " . readAll($fp);
+
 fwrite($fp, "QUIT\r\n");
+echo "QUIT: " . readAll($fp);
 fclose($fp);
